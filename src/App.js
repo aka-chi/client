59cc72cbadc1c10a8b8a04a848953371369e2db4
akachi <46725424+aka-chi@users.noreply.github.com>
2022-06-23 13:21:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default function Create() {
    // When a post request is sent to the create url, we'll add a new record to the database.
    const newPerson = { ...form };
  
-   await fetch("https://sererver.herokuapp.com//record/add", {
+   await fetch("https://sererver.herokuapp.com/record/add", {
      method: "POST",
      headers: {
        "Content-Type": "application/json",
